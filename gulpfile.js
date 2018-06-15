96924cacefe6715d15a803ac2d4c2a50a5463d89
Bryan Ramos <b.r.tech95@gmail.com>
2018-06-15 05:46:40 -0400

    New files, post-css added to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'); //the gulp package
+//gulp-watch package, runs functions when it senses file changes.
+var watch = require('gulp-watch'); 
 
+
+//default gulp tasks, runs by just typing gulp into the terminal
 gulp.task('default', function(){
 	console.log("IT WORKS MY GUY");
 });
@@ -10,9 +13,32 @@ gulp.task('html', function(){
 	console.log("I'm doing something to your html rn...");
 });
 
+gulp.task('styles', function(){
+	return gulp.src("./app/assets/styles/styles.css")
+		.pipe()
+		.pipe(gulp.dest('./app/temp/styles'))
+
+});
+
 
 gulp.task('watch', function(){
     watch('./app/index.html', function(){
     	gulp.start('html');
-    });
+	});
+	
+	watch('./app/assets/styles/**/*.css',function(){
+		gulp.start('styles');
+	});
 });
+
+
+//Gulp notes
+/*
+	gulp.src() -> The source of the files we want to manipulate
+
+	gulp.dest() -> Where we want to send these files
+
+	.pipe() -> Used to manipulate the file as it moves to its destination
+
+
+*/
